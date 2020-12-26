f73d2aad6b9c3ff59c57b277c2eb9210ff00d123
Artchitect <adrianlrichardson@gmail.com>
2020-12-26 16:05:16 -0600

    final changes edit

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,9 +1,13 @@
+//import { from } from "rxjs";
+import { stringify } from 'querystring';
+
 export class Satellite {
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
+    
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
@@ -11,4 +15,8 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
-}  
+    shouldShowWarning(): boolean {
+        return this.type === "Space Debris";
+            
+    }
+} 
